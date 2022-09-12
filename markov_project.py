5c2611888496249671938cfcc31efb685e90ca46
Michael Webber <mwebber@bowdoin.edu>
2022-09-12 15:14:58 -0400

    Start of Image Selection

--- markov_project.py
+++ markov_project.py
@@ -1,28 +1,24 @@
 """
-Learning Objectives
-- Let's remember Python!
-- What would a Markov chain look like in code?
-Example of an Markov Chain implementation in Python (using concepts we learned
-in 1101).
-Dependencies: numpy, scipy
+Image Selection Program
+There are probablities linked with the transition from one 
+image to the next image. The user can specify how many images
+they would like to have in their "post". Using the transisition
+we will output the number of pictures. The picture type will
+depend on the probabilities of it being selected
 """
 import numpy as np
 from scipy.io.wavfile import write
-SAMPLE_RATE = 44100
-# We can add more of these later...keeping it simple for now!
-NOTE_FREQUENCIES = {
-    "A": 440.01,
-    "B": 493.89,
-    "C": 261.63
-}
-class MarkovMusician:
+
+class ImageSelection:
+
     def __init__(self, transition_matrix):
         """Simulates a musician that relies on a simple Markov chain.
            Args:
                 transition_matrix (dict): transition probabilities
         """
         self.transition_matrix = transition_matrix
-        self.notes = list(transition_matrix.keys())
+        self.image_type = list(transition_matrix.keys())
+
     def get_next_note(self, current_note):
         """Decides which note to play next based on the current note.
            Args:
@@ -33,6 +29,7 @@ class MarkovMusician:
             p=[self.transition_matrix[current_note][next_note] \
             for next_note in self.notes]
         )
+
     def compose_melody(self, current_note="A", song_length=3):
         """Generates a sequence of notes.
            Args:
@@ -47,6 +44,7 @@ class MarkovMusician:
             melody.append(next_note)
             current_note = next_note
         return melody
+
     def get_wave(self, frequency=440, duration=0.3, max_amplitude=4096):
         """Retrieve sound wave for note based on given args...
            Args:
@@ -59,6 +57,7 @@ class MarkovMusician:
         sound_wave = max_amplitude * np.sin(2 * np.pi * frequency * time)
         # print("Sound Wave: ", sound_wave)
         return sound_wave
+
     def get_sound_waves_for(self, melody):
         """Transforms the string melody into a list of sound waves.
            Args:
@@ -72,6 +71,7 @@ class MarkovMusician:
         song = np.concatenate(song)
         # print("Song is now", song)
         return song
+
     def write_sound_file(self, melody):
         """Write out a collection of sound waves (i.e., a song!) to a file.
            Args:
@@ -79,16 +79,20 @@ class MarkovMusician:
         """
         sound_data = self.get_sound_waves_for(melody)
         write("new-hit-song.wav", SAMPLE_RATE, sound_data.astype(np.int16))
+
 def main():
+
     song_maker = MarkovMusician({
-        "A": {"A": 0.3, "B": 0.4, "C": 0.3},
-        "B": {"A": 0.7, "B": 0.2, "C": 0.1},
-        "C": {"A": 0.1, "B": 0.7, "C": 0.2}
+        "Kicking": {"Kicking": 0.2, "Standing": 0.5, "Catching": 0.3},
+        "Standing": {"Kicking": 0.4, "Standing": 0.1, "Catching": 0.5},
+        "Catching": {"Kicking": 0.4, "Standing": 0.4, "Catching": 0.2}
     })
-    new_song = song_maker.compose_melody(current_note="C", song_length=10)
+    
+    '''new_song = song_maker.compose_melody(current_note="C", song_length=10)
     print("Here's my latest song:", new_song)
     print("Writing song to file...")
     song_maker.write_sound_file(new_song)
-    print("Process completed!")
+    print("Process completed!")'''
+
 if __name__ == "__main__":
     main()
